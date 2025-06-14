0f4adde4fa3c5c0fa02f20e2609953547bc36d66
vicky16032205 <vickyguptagkp55@gmail.com>
2025-06-14 20:31:01 +0530

    Prime number calculation

--- Questions_Python/QuestionOnLoops/ques8.py
+++ Questions_Python/QuestionOnLoops/ques8.py
@@ -1,9 +1,16 @@
 num = int(input("Enter a number: "))
-flag = False
-for i in range(2,num):
-    if num%i == 0:
-        print("Not a prime number")
-        flag = True
-        break
-if not flag:
-    print("Prime number")
+
+# Handle edge cases
+if num <= 1:
+    print("Not a prime number")
+else:
+    flag = False
+    # Check divisibility from 2 to square root of num
+    upper_limit = int(num ** 0.5) + 1
+    for i in range(2, upper_limit):
+        if num % i == 0:
+            print("Not a prime number")
+            flag = True
+            break
+    if not flag:
+        print("Prime number")
